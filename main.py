ee0595e004eb9efeef34647daba547a60571990a
burakhanaksoy <burakhan.aksoy@tedu.edu.tr>
2021-04-25 14:16:34 +0300

    linked list implementation

--- main.py
+++ main.py
@@ -3,66 +3,54 @@ class Node:
         self.data = data
         self.next = None
 
+    def __repr__(self):
+        return str(self.data)
 
-class linked_list:
+
+class LinkedList:
     def __init__(self):
         self.head = Node()
 
     def append(self, data):
-        new_node = Node(data)
-        cur = self.head
-        while cur.next != None:
-            cur = cur.next
-        cur.next = new_node
-
-    def length(self):
-        cur = self.head
-        total = 0
-        while cur.next != None:
-            total += 1
-            cur = cur.next
-        return total
+        cur_node = self.head
+        while cur_node.next != None:
+            cur_node = cur_node.next
+        cur_node.next = Node(data)
 
     def display(self):
         elems = []
         cur_node = self.head
         while cur_node.next != None:
             cur_node = cur_node.next
-            elems.append(cur_node.data)
-
+            elems.append(cur_node)
         print(elems)
 
-    def get(self, index):
-        if index >= self.length():
-            print('Index can\'t be greater than length..')
-            return
+    @property
+    def length(self):
+        count = 0
         cur_node = self.head
+        while cur_node.next != None:
+            cur_node = cur_node.next
+            count += 1
+        return count
+
+    def remove(self, index):
+        cur_node = self.head
+        prev_node = self.head
         cur_idx = 0
+        while cur_node.next != None:
+            cur_node = cur_node.next
+            if cur_idx == index:
+                prev_node.next = cur_node.next
+                break
+            cur_idx += 1
+            prev_node = cur_node
+
+    def get(self, index):
+        cur_idx = 0
+        cur_node = self.head
         while cur_node.next != None:
             cur_node = cur_node.next
             if cur_idx == index:
                 return cur_node.data
             cur_idx += 1
-
-    def erase(self, index):
-        if index >= self.length():
-            print('Index can\'t be greater than length..')
-            return
-        cur_node = self.head
-        cur_idx = 0
-        while cur_node.next != None:
-            last_node = cur_node
-            cur_node = cur_node.next
-            if cur_idx == index:
-                last_node.next = cur_node.next
-                return
-            cur_idx += 1
-
-
-my_list = linked_list()
-my_list.append(1)
-my_list.append(2)
-my_list.append(3)
-my_list.display()
-my_list.erase(2)
-my_list.display()
